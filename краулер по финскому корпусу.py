db660b61c3c4ec3913990a787d88dec483f09a6d
vyhuholl <olga-p-98@mail.ru>
2018-01-27 16:29:55 +0300

    Update краулер по финскому корпусу.py

--- краулер по финскому корпусу.py
+++ краулер по финскому корпусу.py
@@ -4,11 +4,11 @@ import xlsxwriter
 
 def download_page(pageUrl):
     page = urllib.request.urlopen(pageUrl)
-    text = page.read().decode('utf-8')
+    text = page.read().decode('utf-16')
     return text
 
 commonUrl = 'http://aranea.juls.savba.sk/guest/run.cgi/first?corpname=AranFinn_x&reload=&iquery=&queryselector=cqlrow&lemma=&lpos=&phrase=&word=&wpos=&char=&cql='
-wordlist_sharp = ['vihlova', 'tarkka', 'korotettu', 'kipakka']
+wordlist_sharp = ['terävä', 'jyrkkä', 'vihlova', 'tarkka', 'korotettu', 'kirpeä', 'kipakka', 'teräväkulmainen']
 regTag = re.compile('<.*?>', flags = re.DOTALL)
 
 questionnaire = open('questionnaire.xlsx', 'w', encoding = 'cp1251')
@@ -19,10 +19,8 @@ with open('nouns sharp.txt', encoding = 'cp1251') as wordlist:
         word = word[:-1]
         for sharp in wordlist_sharp:
             url = commonUrl + '%5Blemma+%3D+%22' + sharp + '%22%5D+%5Blemma+%3D+%22' + word + '%22%5D&default_attr=word'
-            print(url)
             page = download_page(url)
             number = re.search('"(.*?) occurrences', page)
-            print(number)
             try:
                 number = number[1:]
                 number = number[:-12]
