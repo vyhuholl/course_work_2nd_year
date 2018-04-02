ad1e2184a67e3a2eeecc2122c5c506f9215d0ba1
vyhuholl <olga-p-98@mail.ru>
2018-04-02 17:58:52 +0300

    Update краулер по финскому корпусу.py

--- краулер по финскому корпусу.py
+++ краулер по финскому корпусу.py
@@ -1,14 +1,15 @@
 import urllib.request
 import re
 import xlsxwriter
+import codecs
 
 def download_page(pageUrl):
     page = urllib.request.urlopen(pageUrl)
-    text = page.read().decode('utf-16')
+    text = codecs.decode(page.read(), encoding = 'utf-8')
     return text
 
 commonUrl = 'http://aranea.juls.savba.sk/guest/run.cgi/first?corpname=AranFinn_x&reload=&iquery=&queryselector=cqlrow&lemma=&lpos=&phrase=&word=&wpos=&char=&cql='
-wordlist_sharp = ['terävä', 'jyrkkä', 'vihlova', 'tarkka', 'korotettu', 'kirpeä', 'kipakka', 'teräväkulmainen']
+wordlist_sharp = ['ter%C3%A4v%C3%A4', 'jyrkk%C3%A4', 'vihlova', 'tarkka', 'korotettu', 'kirpe%C3%A4', 'kipakka', 'ter%C3%A4v%C3%A4kulmainen']
 regTag = re.compile('<.*?>', flags = re.DOTALL)
 
 questionnaire = open('questionnaire.xlsx', 'w', encoding = 'cp1251')
@@ -19,8 +20,10 @@ with open('nouns sharp.txt', encoding = 'cp1251') as wordlist:
         word = word[:-1]
         for sharp in wordlist_sharp:
             url = commonUrl + '%5Blemma+%3D+%22' + sharp + '%22%5D+%5Blemma+%3D+%22' + word + '%22%5D&default_attr=word'
+            print(url)
             page = download_page(url)
             number = re.search('"(.*?) occurrences', page)
+            print(number)
             try:
                 number = number[1:]
                 number = number[:-12]
@@ -34,5 +37,4 @@ with open('nouns sharp.txt', encoding = 'cp1251') as wordlist:
 
 wordlist.close()
 questionnaire.close()
-
  
